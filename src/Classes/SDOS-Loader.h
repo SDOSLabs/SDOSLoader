f9753196adb239f5d4e3193fdd6852b3eded0883
Rafael Fernandez Alvarez <rafael.fernandez@sdos.es>
2016-10-14 13:53:26 +0200

    Añadida clase con los imports

---
+++ src/Classes/SDOS-Loader.h
@@ -0,0 +1,16 @@
+//
+//  CoreiOS.h
+//  BaseProject
+//
+//  Created by Rafael Fernandez Alvarez on 20/11/15.
+//  Copyright © 2015 SDOS. All rights reserved.
+//
+
+//Logging
+#import "LoaderManager.h"
+#import "LoaderObject.h"
+#import "GenericLoaderCustomizationProtocol.h"
+
+
+
+
